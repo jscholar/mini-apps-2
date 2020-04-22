f47abfc88782974e8e90e00f754efeef98d6d912
Jin Cai <jinruncai@gmail.com>
2020-04-22 11:39:46 -0700

    Render app to DOM

---
+++ challenge_1/client/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const app = <App />;
+const containerElement = document.getElementById('app');
+
+ReactDOM.render(app, containerElement);
--- challenge_1/webpack.config.js
+++ challenge_1/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'client/app'),
+  entry: path.resolve(__dirname, 'client/index'),
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
