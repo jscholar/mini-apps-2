9b188cc2f311c46f14dd403f5550aa022860c4cc
Jin Cai <jinruncai@gmail.com>
2020-04-22 15:10:47 -0700

    Limit results to 10

--- challenge_1/client/api/events.js
+++ challenge_1/client/api/events.js
@@ -1,12 +1,18 @@
 import axios from 'axios';
 
-const searchEvents = (query) => (
+export const searchEvents = (query, page = 1) => (
   axios({
     url: '/events',
     params: {
       q: query,
+      _page: page,
+      _limit: 10,
     },
   })
+    .then(({ data }) => data)
+    .catch((reason) => {
+      throw new Error('Request failed: ', reason);
+    })
 );
 
 export default {
