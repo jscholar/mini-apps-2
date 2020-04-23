945f3d53e4c58ed6bd4168319a45176bb4d19842
Jin Cai <jinruncai@gmail.com>
2020-04-23 11:13:37 -0700

    React app boilerplate

---
+++ challenge_2/client/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const containerElement = document.getElementById('app');
+const app = <App></App>
+
+ReactDOM.render(app, containerElement);
