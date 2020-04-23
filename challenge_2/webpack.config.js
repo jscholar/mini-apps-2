5054e70e5683b3d3e362906398c3983a29aeafca
Jin Cai <jinruncai@gmail.com>
2020-04-23 10:35:55 -0700

    Install and configure webpack/babel

---
+++ challenge_2/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'client', 'index'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'client')
+        ],
+        exclude: [
+          path.resolve(__dirname, 'node_modules')
+        ],
+        loader: 'babel-loader',
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'inline-source-map',
+};
