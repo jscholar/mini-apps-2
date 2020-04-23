4095212397481076054c78d0aa855d5b5d85b099
Jin Cai <jinruncai@gmail.com>
2020-04-23 10:17:54 -0700

    init express server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,10 @@
+const path = require('path');
+const express = require('express');
 
+const app = express();
 
+app.use('/', express.static(path.resolve(__dirname, 'public')));
+
+app.listen(3000, () => {
+  console.log('express server listening on port 3000')
+});
