2162d0ab7096a2764eb1a64cad066a70d481b129
Jin Cai <jinruncai@gmail.com>
2020-04-24 10:48:16 -0700

    Init react app

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,13 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
 
+const App = () => (
+  <div>
+    Bowling App
+  </div>
+);
 
+const app = <App />;
+const containerElement = document.getElementById('app');
+
+ReactDOM.render(app, containerElement);
